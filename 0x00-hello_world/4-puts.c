226518da639669735b81a04c60633c1b248c7016
Nwonye <nwonyeblossom@gmail.com>
2022-09-08 10:06:51 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Description: prints "Programming is like building a multilingual puzzle"
  *
- * Return : 0 if no error occurs
+ * Return: 0 if no error occurs
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
