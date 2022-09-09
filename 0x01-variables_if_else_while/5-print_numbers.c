56fe0b42efe12b3abe4ca91e8df4a1fbb1ea550a
Nwonye <nwonyeblossom@gmail.com>
2022-09-09 12:43:59 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * Description: "A program that prints all single digit numbers of base 10
+ * starting from 0"
+ *
+ * Return: 0 if no error occurs
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	
+	putchar(10);
+
+	return(0);
+}
