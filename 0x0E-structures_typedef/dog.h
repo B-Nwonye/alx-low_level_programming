5ac53673685a8da848f66e33f6edbff4e33cfcf2
Nwonye <nwonyeblossom@gmail.com>
2022-10-10 06:45:48 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - informs the user about the dog
+ * @name: the dogs name
+ * @age: the dogs age
+ * @owner: the dogs owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
